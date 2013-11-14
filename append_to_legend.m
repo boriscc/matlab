ad6ebe79806a45237883167ff8a8eb0f54a0ebdd
Boris Carlsson <borisc@student.chalmers.se>
2013-11-14 14:16:43 +0100

    Added: append_to_legend
    Added command to append an entry to a legend.

---
+++ append_to_legend.m
@@ -0,0 +1,14 @@
+function append_to_legend(h, name)
+% APPEND_TO_LEGEND  Append a new entry to a legend
+%   APPEND_TO_LEGEND(plot_handle, legend_entry_name)
+%   If there is no legend, a legend will be created.
+%
+% See also LEGEND PLOT
+    [~, ~, outh, outm] = legend;
+    if(isempty(outm))
+        legend(h, name);
+    else
+        legend([outh; h], outm{:}, name);
+    end
+end
+
