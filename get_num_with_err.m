84d7c45d0c7513b716dbc14f74469da06c0c49b9
Boris Carlsson <borisc@chalmers.se>
2014-11-04 19:30:36 +0100

    Added get_num_with_err

---
+++ get_num_with_err.m
@@ -0,0 +1,20 @@
+function str = get_num_with_err(num, err, significant)
+    if nargin < 3
+        significant = 2;
+    end
+    
+    expo = log10(10^significant / err);
+    if expo == floor(expo)
+        expo = floor(expo) - 1;
+    else
+        expo = floor(expo);
+    end
+    err_disp = round(err*10^expo);
+    num_disp = round(num*10^expo)*10^-expo;
+    if expo >= 0
+        form = sprintf('%%+.%df(%%d)', expo);
+        str = sprintf(form, num_disp, err_disp);
+    else
+        str = sprintf('%+*.0f(%d)', significant - expo, num_disp, err_disp*10^-expo);
+    end
+end
