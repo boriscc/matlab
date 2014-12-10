816fcb1220de56245f9f88c8c8a78d00263d6c4a
Boris Carlsson <borisc@chalmers.se>
2014-12-10 20:23:47 +0100

    Updated get_num_with_err

--- get_num_with_err.m
+++ get_num_with_err.m
@@ -1,11 +1,107 @@
-function str = get_num_with_err(num, err, significant)
+function str = get_num_with_err(num, err_down, err_up, num_significant, err_significant)
+    sym = 0;
     if nargin < 3
-        significant = 2;
+        err_up = err_down;
+        err_down = -err_down;
+        sym = 1;
+        num_significant = Inf;
+        err_significant = 2;
     end
-    if imag(err) ~= 0
+    if nargin < 4
+        num_significant = Inf;
+        err_significant = 2;
+    end
+    if nargin < 5
+        err_significant = 2;
+    end
+    if imag(err_down) ~= 0 || imag(err_up) ~= 0
         error('Got imaginary error');
     end
+    if -err_down == err_up
+        sym = 1;
+    end
     
+    if sym == 0
+        assert(err_down <= 0 && err_up >= 0, 'Bad error limits');
+    end
+    
+    significant = min([get_level(max([-err_down, err_up])) - get_level(num) + num_significant, err_significant]);
+    
+    if significant <= 0
+        level = get_level(num);
+        num = 10^(level+1-num_significant)*round(num*10^(num_significant-level-1));
+        if level+1-num_significant >= 0
+            str = sprintf('%+.0f', num);
+        else
+            form = sprintf('%%+.%df', num_significant - level - 1);
+            str = sprintf(form, num);
+        end
+        return;
+    end
+    
+    if sym == 1
+        err = err_up;
+        assert(err > 0, 'Bad error');
+
+        [ expo, err_disp, num_disp ] = get_disp(significant, err, num);
+        if expo >= 0
+            form = sprintf('%%+.%df(%%d)', expo);
+            str = sprintf(form, num_disp, err_disp);
+        else
+            str = sprintf('%+*.0f(%d)', significant - expo, num_disp, err_disp*10^-expo);
+        end
+    else
+        assert(err_up > err_down, 'Must have one error with |err| > 0');
+        if err_up == 0
+            [ expo, err_disp, num_disp ] = get_disp(significant, -err_down, num);
+            if expo >= 0
+                form = sprintf('%%+.%df_{(-%%d)}^{(+0)}', expo);
+                str = sprintf(form, num_disp, err_disp);
+            else
+                str = sprintf('%+*.0f_{(-%d)}^{(+0)}', significant - expo, num_disp, err_disp*10^-expo);
+            end
+        elseif err_down == 0
+            [ expo, err_disp, num_disp ] = get_disp(significant, err_up, num);
+            if expo >= 0
+                form = sprintf('%%+.%df_{(-0)}^{(+%%d)}', expo);
+                str = sprintf(form, num_disp, err_disp);
+            else
+                str = sprintf('%+*.0f_{(-0)}^{(+%d)}', significant - expo, num_disp, err_disp*10^-expo);
+            end
+        else
+            if -err_down < err_up
+                err_max = err_up;
+            else
+                err_max = -err_down;
+            end
+            [ expo, err_disp, num_disp ] = get_disp(significant, err_max, num);
+            if -err_down > err_up
+                err_down_disp = err_disp;
+                err_up_disp = round(err_up*10^expo);
+            else
+                err_up_disp = err_disp;
+                err_down_disp = -round(err_down*10^expo);
+            end
+            if expo >= 0
+                if err_up_disp == err_down_disp
+                    form = sprintf('%%+.%df(%%%dd)', expo, err_significant);
+                    str = sprintf(form, num_disp, err_up_disp);
+                else
+                    form = sprintf('%%+.%df_{(-%%%dd)}^{(+%%%dd)}', expo, err_significant, err_significant);
+                    str = sprintf(form, num_disp, err_down_disp, err_up_disp);
+                end
+            else
+                if err_up_disp == err_down_disp
+                    str = sprintf('%+*.0f(%d)', significant - expo, num_disp, err_up_disp*10^-expo);
+                else
+                    str = sprintf('%+*.0f_{(-%d)}^{(+%d)}', significant - expo, num_disp, err_down_disp*10^-expo, err_up_disp*10^-expo);
+                end
+            end
+        end
+    end
+end
+
+function [expo, err_disp, num_disp] = get_disp(significant, err, num)
     expo = log10(10^significant / err);
     if expo == floor(expo)
         expo = floor(expo) - 1;
@@ -14,10 +110,15 @@ function str = get_num_with_err(num, err, significant)
     end
     err_disp = round(err*10^expo);
     num_disp = round(num*10^expo)*10^-expo;
-    if expo >= 0
-        form = sprintf('%%+.%df(%%d)', expo);
-        str = sprintf(form, num_disp, err_disp);
+end
+
+function level = get_level(x)
+    x = abs(x);
+    if x == 0
+        level = 0;
+    elseif x < 1
+        level = floor(log10(x));
     else
-        str = sprintf('%+*.0f(%d)', significant - expo, num_disp, err_disp*10^-expo);
+        level = floor(log10(x));
     end
 end
